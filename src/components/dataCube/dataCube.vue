c75da3ee72ceba20adce4d4cfbf37c83ffc2577a
AB786883603 <786883603@qq.com>
2018-03-04 14:21:56 +0800

    引入 echarts 模块

--- src/components/dataCube/dataCube.vue
+++ src/components/dataCube/dataCube.vue
@@ -51,14 +51,42 @@
     	        </div>
     	    </div>
     	</div>
-    	<div id="dataTable" style="width: 10rem;height: 10rem; margin-top:1em">
+    	<div id="dataTable" style="width: 10rem;height: 10rem; margin-top:1em" ref="datatablehook">
     	</div>
 	</div>
 </template>
 
 <script>
-export default {
+import echarts from "echarts";
 
+export default {
+  created: function() {
+    this.$nextTick(() => {
+      let dom = this.$refs.datatablehook
+      var myChart = echarts.init(dom);
+      // 指定图表的配置项和数据
+      var option = {
+          title: {
+              text: 'ECharts 入门示例'
+          },
+          tooltip: {},
+          legend: {
+              data:['销量']
+          },
+          xAxis: {
+              data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+          },
+          yAxis: {},
+          series: [{
+              name: '销量',
+              type: 'bar',
+              data: [5, 20, 36, 10, 10, 20]
+          }]
+      };
+      // 使用刚指定的配置项和数据显示图表。
+      myChart.setOption(option);
+    })
+  }
 }
 </script>
 
